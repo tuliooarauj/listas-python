836149cad88bbfe47c149159fe38626ab2459b54
Tulio Oliveira <127243520+tuliooarauj@users.noreply.github.com>
2023-07-14 09:51:27 -0300

    Update questao9.py

--- lista3/questao9.py
+++ lista3/questao9.py
@@ -3,47 +3,45 @@ qtd_entradas_seguintes = int(input())
 relacao_posicao_treino = []
 minutos_posicao = []
 
+venceu = False
+
 for entrada in range(qtd_entradas_seguintes):
     posicao_min_treino = input().split(' ')   
     relacao_posicao_treino.append(posicao_min_treino)
 for relacao in relacao_posicao_treino:
         minutos_posicao.append(int(relacao[1]))
 
+for num in range(qtd_entradas_seguintes, -1, -1):
+    if num == qtd_entradas_seguintes:
+        fat = num - 0
+    if num != qtd_entradas_seguintes:
+        fat += num
+
+
+
 if len(minutos_posicao) == 1:
     if minutos_posicao[0] == qtd_ideal_treino:
-     print(f'Conquistamos nossa primeira estrela! Barbie e Chelsea arrasaram nos treinos das {relacao_posicao_treino[0][0]}!')
+        venceu = True 
+        print(f'Conquistamos nossa primeira estrela! Barbie e Chelsea arrasaram nos treinos das {relacao_posicao_treino[0][0]}!')
 
 if len(minutos_posicao) > 1:
     lista_resultado = []
-    for idx in range(1, qtd_entradas_seguintes):
+    for idx in range(1, qtd_entradas_seguintes): #range !qtd_entradas
         if idx == 1:
             soma_treinamento = minutos_posicao[0] + minutos_posicao[idx]
+            lista_resultado.append(relacao_posicao_treino[0][0])
             if soma_treinamento == qtd_ideal_treino:
                 lista_resultado.append(relacao_posicao_treino[idx][0])
-                print(f'Conquistamos nossa primeira estrela! Barbie e Chelsea arrasaram nos treinos das {relacao_posicao_treino[idx][0]}!')
+                venceu = True
+                print(f'Conquistamos nossa primeira estrela! Barbie e Chelsea arrasaram nos treinos das {lista_resultado}')
+
         if idx > 1:
+            lista_resultado.append(relacao_posicao_treino[1][0])
             soma_treinamento += minutos_posicao[idx]
             if soma_treinamento == qtd_ideal_treino:
-                print(f'Conquistamos nossa primeira estrela! Barbie e Chelsea arrasaram nos treinos das {relacao[0]}!')
+                lista_resultado.append(relacao_posicao_treino[idx][0])
+                venceu = True
+                print(f'Conquistamos nossa primeira estrela! Barbie e Chelsea arrasaram nos treinos das {lista_resultado}')
 
-#x valor almejado
-
-  #  dado, a, b, c, d
-
-  #  testar 
-  #  se a = x
-   # se a + b = x 
-  #  se a + b + c = x
-  #  se a + b + c + d = x
-
-   # se b = x
-   # se b + c = x
-    #se b + c + d = x
-
-   # se c = x
-    #se c + d = x
-
-    #se d = x
-#
-
-
+if not venceu:
+    print('Não treinamos na dose certa e infelizmente a estrela vai ter que ficar para a próxima')    
