8c914e08f2d2c6ab1b86db990bad4b4e4bee1ca4
Tulio Oliveira <tulio_oliveira27@hotmail.com>
2023-07-24 11:53:21 -0300

    Update questao2.py

--- lista4/questao2.py
+++ lista4/questao2.py
@@ -5,7 +5,6 @@ def relatorio_participante(nome, qtd_propulsor, v_inicial, v_final):
     print(f'Velocidade Inicial: {v_inicial} km/h')
     print(f'Velocidade Final: {v_final} km/h')
 
-situacao = 'Próximo'
 
 situacao1 = 'Pit-Stop Espacial'
 situacao2 = 'Um Droide apareceu do nada na pista, do nadaaa! Perdi o controle e bati em uma pedra.'
@@ -16,42 +15,59 @@ lista_participantes = []
 idx = 0
 corrida_finalizada = False
 participante_desclassificado = 0
+participante_removido = False
 
 while not corrida_finalizada:
+    situacao = ''
 
-    if situacao == 'Próximo':
+    if participante_removido:
+        proxima_decisao = input() #proxima decisao pq pode estar se referenciando à situação ou dados do prox participante
+        if proxima_decisao == 'FIM':
+            corrida_finalizada = True
+        else: #não está referenciando à fim, logo está referenciando aos dados do prox participante
+            participante_removido = False
+            informacoes_participante = proxima_decisao.split()
+            lista_participantes.append(informacoes_participante)
+            propulsor_participante = int(informacoes_participante[1])
+    else: #caso padrão
+        participante_removido = False
         informacoes_participante = input().split()
         lista_participantes.append(informacoes_participante)
-        idx += 1
-        propulsor_participante = int(informacoes_participante[1])
+        propulsor_participante = int(informacoes_participante[1])       
 
-    situacao = input()
+    while not situacao == 'Próximo' and not situacao == 'FIM' and not participante_removido: #condições que a rodada não acaba
 
-    if situacao == 'Próximo' or situacao == 'FIM':
-        velocidade_inicial = int(informacoes_participante[1]) * int(informacoes_participante[2])
-        velocidade_final = int(propulsor_participante) * int(informacoes_participante[2])
+        situacao = input()
 
-        relatorio_participante(informacoes_participante[0], propulsor_participante, velocidade_inicial, velocidade_final)
 
-    if situacao == situacao1:
-        propulsor_participante += 1
-    elif situacao == situacao2:
-        propulsor_participante -= 1
-        if propulsor_participante == 0:
-            participante_desclassificado += 1
-            print(f'BUUMM!! Infelizmente, {informacoes_participante[0]} está fora da corrida.')
+        if situacao == situacao1:
+            propulsor_participante += 1
+        elif situacao == situacao2:
+            propulsor_participante -= 1
+            if propulsor_participante == 0:
+                participante_desclassificado += 1
+                print(f'BUUMM!! Infelizmente, {informacoes_participante[0]} está fora da corrida.')
+                lista_participantes.remove(informacoes_participante)
+                participante_removido = True
+
+        elif situacao == situacao3:
             lista_participantes.remove(informacoes_participante)
-            situacao = 'Próximo'
-    elif situacao == situacao3:
-        lista_participantes.remove(informacoes_participante)
-        print(f'O {informacoes_participante[0]} achou que não descobriríamos, tirem {informacoes_participante[0]} imediatamente da pista.')
-        participante_desclassificado += 1
-        situacao = 'Próximo'
+            print(f'O {informacoes_participante[0]} achou que não descobriríamos, tirem {informacoes_participante[0]} imediatamente da pista.')
+            participante_desclassificado += 1
+            participante_removido = True
 
-    elif situacao == 'FIM':
-        corrida_finalizada = True
+        elif situacao == 'FIM':
+            corrida_finalizada = True
 
+    if not participante_removido:
+        if situacao == 'Próximo' or situacao == 'FIM':
+            velocidade_inicial = int(informacoes_participante[1]) * int(informacoes_participante[2])
+            velocidade_final = int(propulsor_participante) * int(informacoes_participante[2])
+
+            relatorio_participante(informacoes_participante[0], propulsor_participante, velocidade_inicial, velocidade_final)
+        
 concluintes = (len(lista_participantes) + participante_desclassificado) - participante_desclassificado #total_participantes - desclassificados
+
 
 if concluintes >= 1:
     print(f'Relatório da CIn Pod Race: {concluintes} participantes terminaram a corrida e {participante_desclassificado} foram desclassificados.')
