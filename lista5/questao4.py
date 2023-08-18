759a0656e778f85b9ed52038e2e92cc7bf304197
Tulio Oliveira <tulio_oliveira27@hotmail.com>
2023-08-17 21:13:28 -0300

    Update questao4.py

--- lista5/questao4.py
+++ lista5/questao4.py
@@ -1,11 +1,16 @@
 def verifica_grau(string_grau_coeficiente, lista_grau_coeficiente, grau_polinomio):
-    if len(lista_grau_coeficiente) < 1:
-        caracter = string_grau_coeficiente[:len(grau_polinomio)]
+    if not string_grau_coeficiente[:1] == ' ':
+        caracter = string_grau_coeficiente[:1]
         if caracter.isnumeric():
             lista_grau_coeficiente.append(int(caracter))
         return verifica_grau(string_grau_coeficiente[1:], lista_grau_coeficiente, grau_polinomio)
     else:
-        return lista_grau_coeficiente[0]
+        if len(lista_grau_coeficiente) > 1:
+            string_lista = [str(numero) for numero in lista_grau_coeficiente]
+            string_numeros_formados = ''.join(string_lista)
+            return int(string_numeros_formados)
+        else:
+            return lista_grau_coeficiente[0]
 
 def retorna_ultimos_4digitos(string_grau_coeficiente):
     return string_grau_coeficiente[:-5:-1] #coeficiente varia de (-1000, 1000)
@@ -23,20 +28,24 @@ def verifica_coeficiente(string_grau_coeficiente_modificada, lista_coeficiente):
 
         
 def monta_monomio(coeficiente, grau):
+    if coeficiente == []:
+        coeficiente = 0
     if coeficiente == 0:
         return '0'
+    elif grau == 0:
+        return str(coeficiente)
     elif coeficiente == 1:
         return 'x^'+str(grau)
     elif coeficiente == -1:
         return '-x^'+str(grau)
-    elif grau == 0:
-        return str(coeficiente)
     elif grau == 1:
         return str(coeficiente) + 'x'
     else:
         return str(coeficiente)+'x^'+str(grau)
     
 def monta_derivada_monomio(monomio, coeficiente, grau, ordem):
+    if coeficiente == []:
+        coeficiente = 0
     if monomio == str(coeficiente):
         return '0'
     elif monomio == str(coeficiente)+'x':
@@ -72,33 +81,44 @@ derivadas = []
 coeficientes_ordenados = cria_lista_coeficientes([], grau_polinomio)
 
 for _ in range(coeficientes_nao_nulos):
+
     grau_e_coeficiente = input()   
-    grau = verifica_grau(grau_e_coeficiente, [], grau_polinomio)
+    grau = verifica_grau(grau_e_coeficiente[15:], [], grau_polinomio)
     coeficiente = verifica_coeficiente(retorna_ultimos_4digitos(grau_e_coeficiente), [])
     ordena_coeficientes_polinomio(coeficiente, int(grau), coeficientes_ordenados)
 
+i = 0
 for coeficiente in coeficientes_ordenados:
+    
     monomio = monta_monomio(coeficiente, coeficientes_ordenados.index(coeficiente))
     monomios.append(monomio)
     derivada = monta_derivada_monomio(monomio, coeficiente, coeficientes_ordenados.index(coeficiente), ordem_derivada)
+
     if len(derivada) > 1:
         coeficiente = int(derivada[0])
         grau = int(derivada[1])
         monomio_derivado = monta_monomio(coeficiente, grau)
         derivadas.append(monomio_derivado)
+        coeficientes_ordenados[i] = []
     else:
         coeficiente = int(derivada)
         monomio_derivado = monta_monomio(coeficiente, grau)
         derivadas.append(monomio_derivado)
+        coeficientes_ordenados[i] = []
+    i+=1
 
 if len(monomios) > 1:
     for elemento in monomios[:]:
         if elemento == '0' or elemento == '[]':
             monomios.remove(elemento)
+    if monomios == []:
+        monomios.append('0')
 if len(derivadas) > 1:
     for elemento in derivadas[:]:
         if elemento == '0':
             derivadas.remove(elemento)
+    if derivadas == []:
+        derivadas.append('0')
 
 
 polinomio = '+'.join(monomios)
@@ -108,6 +128,9 @@ polinomio_FPB = polinomio.replace('+-','-',aparicoes_erradas_polinomio)
 polinomio_derivado = '+'.join(derivadas)
 aparicoes_erradas_derivadas = polinomio_derivado.count('+-')
 polinomio_derivado_FPB = polinomio_derivado.replace('+-','-',aparicoes_erradas_derivadas)
+
+if ordem_derivada == 0:
+    polinomio_derivado_FPB = polinomio_FPB
 
 
 print(f'A derivada {ordem_derivada} do polinômio {polinomio_FPB} é')
