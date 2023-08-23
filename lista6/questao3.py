98f41971d68631b495890c719fb4d940e3ace800
Tulio Oliveira <127243520+tuliooarauj@users.noreply.github.com>
2023-08-23 12:25:07 -0300

    Update questao3.py

--- lista6/questao3.py
+++ lista6/questao3.py
@@ -15,16 +15,18 @@ def cria_fila():
     return fila
 
 def verifica_possibilidade(qtd_frente, fila):
-    if qtd_frente == len(fila) + 1:
+    if qtd_frente == len(fila):
         return True
     else:
         return False
 
 def remove_zeros_esquerda(numero):
-    if int(numero[:1]) != 0:
-        return int(numero)
-    else:
-        return remove_zeros_esquerda(numero[1:])
+    if len(numero) > 1:
+        if int(numero[:1]) != 0:
+            return int(numero)
+        else:
+            return remove_zeros_esquerda(numero[1:])
+    return int(numero)
 
 def verifica_concatenacao(dialeto):
     if '+' in dialeto:
@@ -41,17 +43,25 @@ def cria_num_concatenacao(dialeto, dialetos): #string e dicionario
     numero_formado = ''.join(lista_aux)
     return numero_formado
 
-remove_zeros_esquerda('01')
-
-
-
-'''qtd_pessoas = int(input())
+possivel = True
+qtd_pessoas = int(input())
 for pessoas in range(qtd_pessoas):
     dialeto = input()
-    
-    if dialetos[dialeto] == 0:
+    if verifica_concatenacao(dialeto):
+        numero = remove_zeros_esquerda(cria_num_concatenacao(dialeto, dialetos))
+    else:
+        numero = remove_zeros_esquerda(str(dialetos[dialeto]))
+
+    if pessoas == 0 or numero == 0:
         fila = cria_fila()
     else:
         if verifica_possibilidade(dialetos[dialeto], fila):
             pos = len(fila)+1
-            fila.update({pos: dialetos[dialeto]})'''
+            fila.update({pos: dialetos[dialeto]})
+        else:
+            possivel = False
+
+if possivel:
+    print('YES')
+else:
+    print('NO')
