4747980cda50b28ff15fb7f39456714862010b1b
Tulio Oliveira <tulio_oliveira27@hotmail.com>
2023-08-25 18:11:57 -0300

    Update questao5.py

--- lista6/questao5.py
+++ lista6/questao5.py
@@ -35,25 +35,33 @@ def julgamento(nome, banco_de_dados):
     indicio_fakenatty = 0
     for caracteristica, valor in banco_de_dados[nome].items():
         if caracteristica == 'Biceps':
-            valor = valor.replace('cm','')
-            if int(valor) > 45:
-                indicio_fakenatty += 1
+            if not valor == '':
+                valor = valor.replace('cm','')
+                if int(valor) > 45:
+                    indicio_fakenatty += 1
+
         elif caracteristica == 'Treino':
-            valor = converte_hora_seg_min(valor)
-            if valor < 30:
-                indicio_fakenatty += 1
+            if not valor == '':
+                valor = converte_hora_seg_min(valor)
+                if valor < 30:
+                    indicio_fakenatty += 1
+
         elif caracteristica == 'Frequencia':
-            valor = valor.replace('dias','')
-            valor = valor.replace('dia','')
-            if int(valor) < 4:
-                indicio_fakenatty += 1
+            if not valor == '':
+                valor = valor.replace('dias','')
+                valor = valor.replace('dia','')
+                if int(valor) < 4:
+                    indicio_fakenatty += 1
+
         elif caracteristica == 'BF':
             if not valor == '':
                 valor = remove_porcentagem(valor)
                 if valor < 10:
                     indicio_fakenatty+=1
-        elif caracteristica == 'Suor' and (valor == 'Seco' or valor == ''):
+
+        elif caracteristica == 'Suor' and (valor == 'Seco' or valor == '') :
             indicio_fakenatty +=1
+
     if indicio_fakenatty >= 3:
         return True
     else:
@@ -68,10 +76,48 @@ def nattymeter(banco_de_dados):
         return None
     else:
         return qtd_fakenatty / len(banco_de_dados)*100
+    
+entrada = ''
+while not entrada == 'FIM':
+    entrada = input()
 
-adicionar_suspeito('Rodrigo Goes', goes_db)
-adicionar_suspeito('Rayane Gomes', goes_db)
-atualizar_suspeito('Rodrigo Goes',  'Biceps: 35cm, Treino: 1 hora, Frequencia: 5 dias, BF: 12%', goes_db)
-atualizar_suspeito('Rayane Gomes', 'Biceps: 50cm, Treino: 120 segundos, Frequencia: 1 dia', goes_db)
+    if entrada == 'Adicionar suspeito':
+        nome_suspeito = input()
+        adicionar_suspeito(nome_suspeito, goes_db)
+        print(f'Novo suspeito: {nome_suspeito}')
 
-print(nattymeter(goes_db))
+    elif entrada == 'Atualizar suspeito':
+        nome_suspeito, caracteristicas = input().split('-> ')
+        if not nome_suspeito in goes_db:
+            print('Quem é esse crazy man? Não tá aqui na database')
+        else: 
+            atualizar_suspeito(nome_suspeito, caracteristicas, goes_db)
+
+    elif entrada == 'Remover suspeito':
+        nome_suspeito = input()
+        if not nome_suspeito in goes_db:
+            print('Quem é esse crazy man? Não tá aqui na database')
+        else:
+            remover_suspeito(nome_suspeito, goes_db)
+            print(f'{nome_suspeito} removido da lista de suspeitos, está limpo')
+
+    elif entrada == 'Julgamento':
+        nome_suspeito = input()
+        if not nome_suspeito in goes_db:
+            print('Quem é esse crazy man? Não tá aqui na database')
+        else:
+            print(f'Eu já tenho o meu veredito, e o meu veredito, {nome_suspeito}:')
+            if julgamento(nome_suspeito, goes_db):
+                print('FAKE NATTY! USOU O SUCO!')
+            else:
+                print('Natural')
+
+    elif entrada == 'NattyMeter':
+        print('NattyMeter, ON!')
+        resultado_nattymeter = nattymeter(goes_db)
+        if resultado_nattymeter is None:
+            print('Oh yeah, vivemos em uma sociedade sem suco, um great day')
+        else:
+            resultado_nattymeter = int(resultado_nattymeter)
+            resultado_nattymeter = str(resultado_nattymeter)+'%'
+            print(f'NOOO! {resultado_nattymeter} USARAM O SUCO')
