042d4d01581d0a624519bb6987434e57069509d4
Tulio Oliveira <tulio_oliveira27@hotmail.com>
2023-09-06 20:02:25 -0300

    Create estudos_poo.py

---
+++ poo/estudos_poo.py
@@ -0,0 +1,28 @@
+class Conta:
+    def __init__(self, numero, saldo):
+        self.numero = numero
+        self.saldo = saldo
+
+    def creditar(self, saldo):
+        self.saldo += saldo
+    
+    def debitar(self, saldo):
+        self.saldo -= saldo
+
+conta1 = Conta('123-x', 0.00)
+conta2 = Conta('234-y', 100.00)
+
+print('Valor inicial:')
+print(conta1.__dict__)
+print(conta2.__dict__)
+print('=========================')
+
+#REALIZANDO CRÉDITO NAS CONTAS
+
+conta1.creditar(10)
+conta2.debitar(90)
+
+print('APÓS ALTERAÇÕES')
+print(conta1.__dict__)
+print(conta2.__dict__)
+
