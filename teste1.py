576ccb01fd43f9db1a903ca7878503346b94105b
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2023-10-11 16:52:07 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,2 +1,33 @@
-x = ['2']
-print(''.join(x))
+import pygame
+from pygame.locals import *
+
+#define fps
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 600
+screen_height = 800
+
+player_position_x = screen_width / 2
+player_position_y = screen_height - 100
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Space Invaders')
+
+run = True
+while run:
+
+    clock.tick(fps) #limiting to 60 fps
+
+    #event handlers
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    player = pygame.draw.rect(screen, (255, 0, 0), (player_position_x, player_position_y, 50, 50))
+    throttle = pygame.draw.rect(screen, (0, 255, 0), (300, 100, 20, 20))
+
+
+    pygame.display.update()
+
+pygame.quit()
